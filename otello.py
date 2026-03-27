7e6caaa6bc449f1baddddf74b9a7d72daf8fda6d
MelyGM <133719381+MelyGM@users.noreply.github.com>
2026-03-27 01:14:20 -0700

    Implementación del sucesor, terminal y ganancias

--- otello.py
+++ otello.py
@@ -61,10 +61,59 @@ class Otello(js.JuegoZT2):
 
         return jugadas if jugadas else [None]
 
+    def sucesor(self, s, a, j):
+        if a is None:
+            return s
+
+        s = list(s)
+        s[a] = j
+
+        fila = a // 8
+        columna = a % 8
+
+        for df, dc in self.movimientos:
+            if self.hay_captura(tuple(s), fila, columna, df, dc, j):
+                f = fila + df
+                c = columna + dc
+
+                while self.dentro(f, c) and s[self.pos(f, c)] == -j:
+                    s[self.pos(f, c)] = j
+                    f += df
+                    c += dc
+
+        return tuple(s)
+
+    def terminal(self, s):
+        return 0 not in s or (self.jugadas_legales(s, 1) == [None] and self.jugadas_legales(s, -1) == [None])
+
+    def ganancia(self, s):
+        j1 = s.count(1)
+        j2 = s.count(-1)
+
+        if j1 > j2:
+            return 1
+        elif j2 > j1:
+            return -1
+        return 0
+
     def imprimir_tablero(self, s):
-        for i in range(8):
-            fila = s[i*8:(i+1)*8]
-            print(fila)
+        print("\n   ", end="")
+        for c in range(8):
+            print(c, end=" ")
+        print("\n")
+
+        for f in range(8):
+            print(f, end="  ")
+            for c in range(8):
+                val = s[self.pos(f, c)]
+
+                if val == 1:
+                    print("X", end=" ")
+                elif val == -1:
+                    print("O", end=" ")
+                else:
+                    print(".", end=" ")
+            print()
 
 if __name__ == "__main__":
     juego = Otello()
